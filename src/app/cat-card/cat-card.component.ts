d98dfe9c35bb1dc48735b666ed711968c649d592
Boris <modaxxx009@yahoo.com>
2018-09-26 17:58:13 +0300

    update
    update

--- src/app/cat-card/cat-card.component.ts
+++ src/app/cat-card/cat-card.component.ts
@@ -11,25 +11,100 @@ import { Router } from '@angular/router'
 })
 export class CatCardComponent implements OnInit {
   @Input() cat: Cat;
- 
+    picUrl;
+    index;
+    information;
     
     constructor(private router: Router) {
    
     }
 
   ngOnInit() {
+    this.index=1;
+    this.picUrl=this.cat.imgUrl;
   }
 d
   loadInfo(){
+    this.information = this.cat.information;
     let id = this.cat._id;
     this.router.navigate(['/view-cat/' + id])
   }
   
+  isCastrated(){
+    if(this.cat.castrated){
+      return true;
+    }
+  }
+
+  isVaccinated() {
+    if (this.cat.vaccinated) {
+      return true;
+    }
+  }
+
   isCreator(){
     let user = localStorage.getItem("userId");
     let catCreator = this.cat._acl.creator;
     if(user===catCreator){
       return(true);
+    }
+  }
+
+
+  //SLIDER
+  funcMove(input){
+    let index = this.index;
+    if(input==='left'){
+        func: {
+          let index = this.index;
+          if (index === 1) {
+            this.index = 4;
+            break func;
+          }
+          this.index--;
+        }
+      }
+    else if(input==="right"){
+      func: {
+        let index = this.index;
+        if (index === 4) {
+          this.index = 1;
+          break func;
+        }
+        this.index++;
+      }
+    }
+      
+    
+  }
+  
+
+  //CONDITIONS
+  is1() {
+    let index = this.index;
+    if (index === 1) {
+      return true;
+    }
+  }
+
+  is2() {
+    let index = this.index;
+    if (index === 2) {
+      return true;
+    }
+  }
+
+  is3() {
+    let index = this.index;
+    if (index === 3) {
+      return true;
+    }
+  }
+
+  is4() {
+    let index = this.index;
+    if (index === 4) {
+      return true;
     }
   }
 
